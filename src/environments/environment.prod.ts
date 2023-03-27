3908d57dd2c92e331d1cd01facd0a6c83f2be6d6
Rotiboy95 <polonium209@live.com>
2023-03-27 20:46:04 +0800

    end

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://datingappapi2023.azurewebsites.net/api/',
-    hubUrl: 'https://datingappapi2023.azurewebsites.net/hubs/'
+    // apiUrl: 'https://datingappapi2023.azurewebsites.net/api/',
+    // hubUrl: 'https://datingappapi2023.azurewebsites.net/hubs/'
+    // apiUrl: 'https://localhost:5001/api/',
+    // hubUrl: 'https://localhost:5001/hubs/'
+    apiUrl: 'http://localhost:8080/api/',
+    hubUrl: 'http://localhost:8080/hubs/'
 };
